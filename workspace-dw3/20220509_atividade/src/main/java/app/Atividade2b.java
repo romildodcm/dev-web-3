9feb0f4626bb6bbe2e404b310822271a0cdd092e
romildodcm <romildodcm@gmail.com>
2022-05-17 19:39:54 -0300

    Fixing tomcat9
    Fixing "Server Tomcat v9.0 Server at localhost failed to start."

--- workspace-dw3/20220509_atividade/src/main/java/app/Atividade2b.java
+++ workspace-dw3/20220509_atividade/src/main/java/app/Atividade2b.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/cabecalhos")
+@WebServlet("/d")
 public class Atividade2b extends HttpServlet{
     private static final long serialVersionUID = 80L;
 
