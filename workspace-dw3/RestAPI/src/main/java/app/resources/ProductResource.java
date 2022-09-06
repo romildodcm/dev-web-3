7d3d5e6ac7df0bd1849f7bbd5f66ce87f504d207
Romildo Marques <romildodcm@gmail.com>
2022-09-05 21:55:27 -0300

    aula concluida, tudo rodando

--- workspace-dw3/RestAPI/src/main/java/app/resources/ProductResource.java
+++ workspace-dw3/RestAPI/src/main/java/app/resources/ProductResource.java
@@ -1,11 +1,15 @@
 package app.resources;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,6 +22,7 @@ import app.models.*;
 
 
 @Path("products")
+@Produces(MediaType.APPLICATION_JSON) // todos os metodos, por padrão vao retornar json
 public class ProductResource {
 	//private static final long serialVersionUID = 101L;
 	
@@ -69,5 +74,14 @@ public class ProductResource {
 		}
 		
 		return Response.status(Response.Status.OK).entity(product).build();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createProduct(Product product) throws URISyntaxException {
+		product.setId(4);
+		products.add(product);
+		
+		return Response.created(new URI("/products/" + product.getId())).entity(product).build();
 	}
 }
--- workspace-dw3/RestAPI/src/test/java/app/ProductTest.java
+++ workspace-dw3/RestAPI/src/test/java/app/ProductTest.java
@@ -2,24 +2,69 @@ package app;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import app.models.Product;
+
 public class ProductTest {
+	Client client;
+	WebTarget target;
+	
+	@Before
+	public void setup() {
+		client = ClientBuilder.newClient();
+		target =  client.target("http://localhost:8080/RestAPI/rest");
+	}
+	
+	
 	// testar se o servidor ta ok
+	
 	
 	@Test
 	public void testaSeOServidorTaOn() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target =  client.target("http://localhost:8080/RestAPI/rest");
-		
-		String payload = target.path("/products/1").request().get(String.class);
-		//String payload = target.path("products?sort=desc").request().get(String.class);
-		
-
+		//String payload = target.path("/products/1").request().get(String.class);
+		String payload = target.path("/products").request().get(String.class);
 		Assert.assertTrue(payload.contains("Notebook"));
 	}
 
+	
+	@Test
+	public void testaSeOMetodoGetProductByIdFunciona() throws JsonProcessingException{
+		String payload = target.path("/products/2").request().get(String.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		
+		Product product = objectMapper.readValue(payload, Product.class);
+		Assert.assertEquals("Mouse", product.getProduct());
+				
+	}
+	
+	@Test
+	public void verificaSeOProdutoPodeSerCriado() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Product p = new Product(5, "ProdutoTest", 999.0);
+		String productJson = ow.writeValueAsString(p);
+		
+		Entity<String> productEntity = Entity.entity(productJson, MediaType.APPLICATION_JSON);
+		
+		Response response = target.path("/products").request().post(productEntity);
+		
+		Assert.assertEquals(201, response.getStatus());
+		
+		String path = response.getHeaderString("Location");
+	}
+	
+	
 }
